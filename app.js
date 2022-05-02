a90dec30579a0e948a8b1bc3410598b3cecd98c4
Duong8888 <88546189+Duong8888@users.noreply.github.com>
2022-05-02 22:29:26 +0700

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var count = 0;
+var count1 = 0;
 function non() {
     count++;
     console.log(count);
@@ -21,22 +22,22 @@ function non() {
     }
 }
 function non1() {
-    count++;
+    count1++;
     console.log(count);
     var btn2 = document.querySelector('.main-btn2').style;
-    if (count == 1) {
+    if (count1 == 1) {
         btn2.transform = 'translateX(-300px)';
-    } else if(count == 2) {
+    } else if(count1 == 2) {
         btn2.transform = 'translateY(100px)';
-    } else if(count == 3) {
+    } else if(count1 == 3) {
         btn2.transform = 'translateX(300px)';
-    } else if(count == 4) {
+    } else if(count1 == 4) {
         btn2.transform = 'translateY(-400px)';
-    } else if(count == 5) {
+    } else if(count1 == 5) {
         btn2.transform = 'translateY(100px)';
-    } else if(count == 6) {
+    } else if(count1 == 6) {
         btn2.transform = 'translateX(300px)';
-    } else if(count == 7) {
+    } else if(count1 == 7) {
         btn2.transform = 'translateX(600px)';
         btn2.display = 'none';
     }
