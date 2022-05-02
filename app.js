ce2973e388ec83c21017e2f063c29c8392c482d8
Duong8888 <88546189+Duong8888@users.noreply.github.com>
2022-05-02 22:23:41 +0700

    update

--- app.js
+++ app.js
@@ -20,6 +20,27 @@ function non() {
         btn2.display = 'none';
     }
 }
+function non1() {
+    count++;
+    console.log(count);
+    var btn2 = document.querySelector('.main-btn2').style;
+    if (count == 1) {
+        btn2.transform = 'translateX(-300px)';
+    } else if(count == 2) {
+        btn2.transform = 'translateY(100px)';
+    } else if(count == 3) {
+        btn2.transform = 'translateX(300px)';
+    } else if(count == 4) {
+        btn2.transform = 'translateY(-400px)';
+    } else if(count == 5) {
+        btn2.transform = 'translateY(100px)';
+    } else if(count == 6) {
+        btn2.transform = 'translateX(300px)';
+    } else if(count == 7) {
+        btn2.transform = 'translateX(600px)';
+        btn2.display = 'none';
+    }
+}
 function mo() {
     document.querySelector('.khung').style.zIndex = '10';
     document.querySelector('.thongBao').style.transform = 'scale(1.2)';
